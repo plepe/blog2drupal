11b424a390368d36624320b23b2ac23755cd303c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-01-02 21:04:01 +0100

    Don't create duplicates; Tags

--- run.php
+++ run.php
@@ -14,11 +14,22 @@ $drupal_conf = [
 
 $drupal = new DrupalRestAPI($drupal_conf);
 
+$_categories = $drupal->loadRestExport('/rest/tags');
+$categories = [];
+foreach ($_categories as $c) {
+  $categories[$c['name'][0]['value']] = $c['tid'][0]['value'];
+}
+
+$_articles = $drupal->loadRestExport('/rest/blog');
+$articles = [];
+foreach ($_articles as $c) {
+  $articles[$c['field_id'][0]['value']] = $c['nid'][0]['value'];
+}
+
 $dom = new DOMDocument();
 $dom->loadXML(file_get_contents('http://plepe.at/feed'));
 
 $channel = $dom->firstChild->firstChild->nextSibling;
-print $channel->nodeName . "\n";
 $current = $channel->firstChild;
 while ($current) {
   if ($current->nodeName === 'item') {
@@ -30,15 +41,21 @@ while ($current) {
 
 function processItem ($item) {
   global $drupal;
+  global $articles;
 
   $current = $item->firstChild;
   $data = [
     'type' => [[ 'target_id' => 'article' ]],
+    'field_tags' => [],
   ];
 
   while ($current) {
-    print $current->nodeName . "\n";
-
+    if ($current->nodeName === 'link') {
+      $data['field_id'] = [[ 'value' => $current->textContent ]];
+    }
+    if ($current->nodeName === 'category') {
+      $data['field_tags'][] = [ 'target_id' => getCategory($current->textContent) ];
+    }
     if ($current->nodeName === 'title') {
       $data['title'] = [[ 'value' => $current->textContent ]];
     }
@@ -47,11 +64,33 @@ function processItem ($item) {
       $data['created'] = [[ 'value' => $d->format('c') ]];
     }
     if ($current->nodeName === 'content:encoded') {
-      $data['body'] = [[ 'value' => $current->textContent, 'format' => 'basic_html' ]];
+      $data['body'] = [[ 'value' => $current->textContent, 'format' => 'full_html' ]];
     }
 
     $current = $current->nextSibling;
   }
 
-  $drupal->nodeSave(null, $data);
+  if (!array_key_exists($data['field_id'][0]['value'], $articles)) {
+    $drupal->nodeSave(null, $data);
+  }
+}
+
+function getCategory ($category) {
+  global $categories;
+  global $drupal;
+
+  if (array_key_exists($category, $categories)) {
+    return $categories[$category];
+  }
+
+  $content = [
+    'vid' => [[ 'target_id' => 'tags' ]],
+    'name' => [[ 'value' => $category ]],
+  ];
+
+  print "Creating {$category} ";
+  $content = $drupal->taxonomySave(null, $content);
+  print "-> {$content['tid'][0]['value']}\n";
+
+  $categories[$category] = $content['tid'][0]['value'];
 }
