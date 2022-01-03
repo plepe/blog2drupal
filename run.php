8074d72443606d02915b7431e765c42670889069
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-01-03 22:22:46 +0100

    Parse body of posts to create paragraphs

--- run.php
+++ run.php
@@ -42,6 +42,7 @@ while ($current) {
 function processItem ($item) {
   global $drupal;
   global $articles;
+  $content = null;
 
   $current = $item->firstChild;
   $data = [
@@ -64,14 +65,22 @@ function processItem ($item) {
       $data['created'] = [[ 'value' => $d->format('c') ]];
     }
     if ($current->nodeName === 'content:encoded') {
-      $data['body'] = [[ 'value' => $current->textContent, 'format' => 'full_html' ]];
+      $body = $current->textContent;
     }
 
     $current = $current->nextSibling;
   }
 
   if (!array_key_exists($data['field_id'][0]['value'], $articles)) {
-    $drupal->nodeSave(null, $data);
+    $content = $drupal->nodeSave(null, $data);
+    $id = $content['nid'][0]['value'];
+
+    $data = [
+      'type' => [[ 'target_id' => 'article' ]],
+      'field_content' => parseContent($id, $body),
+    ];
+
+    $content = $drupal->nodeSave($id, $data);
   }
 }
 
@@ -93,4 +102,62 @@ function getCategory ($category) {
   print "-> {$content['tid'][0]['value']}\n";
 
   $categories[$category] = $content['tid'][0]['value'];
+}
+
+function parseContent ($parent_id, $text) {
+  $dom = new DOMDocument();
+  $dom->loadHTML('<?xml encoding="UTF-8"><html><body>' . $text . '</body></html>');
+
+  $result = [];
+
+  $content = '';
+  $current = $dom->getElementsByTagName('body')->item(0)->firstChild;
+  while ($current) {
+    if ($current->nodeName === 'table') {
+      if (trim($content) !== '') {
+        $result[] = createTextParagraph($content);
+        $content = '';
+      }
+
+      $gallery = [];
+      foreach ($current->getElementsByTagName('td') as $td) {
+        $url = $td->getElementsByTagName('a')->item(0)->getAttribute('href');
+        $gallery[] = [
+          'url' => $url,
+          'title' => trim($td->textContent),
+        ];
+      }
+
+      //$result[] = $gallery;
+    } else {
+      $content .= $dom->saveHTML($current);
+    }
+
+    $current = $current->nextSibling;
+  }
+
+  if (trim($content) !== '') {
+    $result[] = createTextParagraph($parent_id, $content);
+  }
+
+  return $result;
+}
+
+function createTextParagraph ($parent_id, $text) {
+  global $drupal;
+
+  $content = [
+    'type' => [[ 'target_id' => 'text_block' ]],
+    'parent_type' => [[ 'value' => 'node' ]],
+    'parent_id' => [[ 'value' => $parent_id ]],
+    'parent_field_name' => [[ 'value' => 'field_content' ]],
+    'field_body' => [[ 'value' => $text, 'format' => 'full_html' ]],
+  ];
+
+  $content = $drupal->paragraphSave(null, $content);
+
+  return [
+    'target_id' => $content['id'][0]['value'],
+    'target_revision_id' => $content['revision_id'][0]['value'],
+  ];
 }
